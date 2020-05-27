6c21dfa98a16651e36d8a03395fa062bafe4cff7
mam-mih-val <mam.mih.val@gmail.com>
2020-05-27 18:58:39 +0200

    Output file name parameter added. Elapsed time calculation added. Excess files removed

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,18 @@
 #include "hades_event_reader.h"
 
 int main(int argv, char **argc) {
-  std::string file_list = argc[1];
+  auto start = std::chrono::system_clock::now();
+  if( argv < 2 ){
+    std::cout << "Error: Incorrect number of arguments. Minimum 1 argument required." << std::endl;
+    std::cout << "Example: ./HT2AT path/to/input.root path/to/output.root" << std::endl;
+    return 1;
+  }
+  std::string file_list;
+  file_list = argc[1];
   std::string file_output{"output.root"};
+  if( argv > 2 )
+    file_output = argc[2];
+
   HadesEventReader reader;
   reader.Init( file_list );
   reader.InitEvtChara( "../evtchara07/centrality_epcorr_apr12_gen8_2019_02_pass30.root" );
@@ -18,5 +28,9 @@ int main(int argv, char **argc) {
   }
   std::cout << i << " events were converted from hades tree to analysis tree." << std::endl;
   Analysis::TreeManager::Instance()->Finalize();
+
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::cout << "Elapsed time: " << elapsed_seconds.count() << " s\n";
   return 0;
 }
