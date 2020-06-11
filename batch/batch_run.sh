720ba0e3039f5083bf525f0844586f4fbd8d0d27
mam-mih-val <mam.mih.val@gmail.com>
2020-06-12 00:25:51 +0300

    batching bug fixed

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -19,8 +19,8 @@ cd $job_num
 echo "loading " $hadesroot
 source $hadesroot
 
-echo "executing $build_dir/HT2AT $input_files"
-$build_dir/HT2AT $input_files
+echo "executing $build_dir/HT2AT --mc $input_files output.dir"
+$build_dir/HT2AT --mc $input_files output.dir
 
 echo JOB FINISHED!
 date $format
