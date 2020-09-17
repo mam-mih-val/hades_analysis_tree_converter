0fd6510c64ce6d38b6e522d4c50b2c3f0dcc5272
mam-mih-val <mam.mih.val@gmail.com>
2020-09-17 15:44:03 +0300

    bus fixed

--- src/hades_event_reader.cc
+++ src/hades_event_reader.cc
@@ -268,7 +268,7 @@ void HadesEventReader::ReadSimData(){
   std::vector<int> selected_tracks;
   for( int i=0; i<geant_kine_->getEntries(); ++i ){
     sim_track = HCategoryManager::getObject(sim_track, geant_kine_, i);
-    int parent_track_id = sim_track->getParent();
+    int parent_track_id = sim_track->getParentTrack();
     bool parent_is_selected = std::count( selected_tracks.begin(), selected_tracks.end(), parent_track_id ) != 0;
     if( !sim_tracks->isPrimary() && !parent_is_selected )
       continue;
