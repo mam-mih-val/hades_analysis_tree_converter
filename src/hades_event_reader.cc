beb089241c69ffb149a395e5b0731a47bce408d6
mam-mih-val <mam.mih.val@gmail.com>
2020-09-17 17:49:14 +0300

    trying to debug

--- src/hades_event_reader.cc
+++ src/hades_event_reader.cc
@@ -270,7 +270,7 @@ void HadesEventReader::ReadSimData(){
   for( int i=0; i<geant_kine_->getEntries(); ++i ){
     sim_track = HCategoryManager::getObject(sim_track, geant_kine_, i);
     int parent_track_id = sim_track->getParentTrack();
-    std::cout << "Track ID: " << sim_track->getTrack() << " Parent ID: " << parent_track_id << std::endl;
+    std::cout << "i=" <<  << " Track ID: " << sim_track->getTrack() << " Parent ID: " << parent_track_id << std::endl;
     bool parent_is_selected = std::count( selected_tracks.begin(), selected_tracks.end(), parent_track_id ) > 0;
     if( !sim_track->isPrimary() && !parent_is_selected )
         continue;
