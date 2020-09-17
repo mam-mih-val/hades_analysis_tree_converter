61da7afe49b131de164a059b88563f9df316a9ca
mam-mih-val <mam.mih.val@gmail.com>
2020-09-17 15:44:55 +0300

    bus fixed

--- src/hades_event_reader.cc
+++ src/hades_event_reader.cc
@@ -270,7 +270,7 @@ void HadesEventReader::ReadSimData(){
     sim_track = HCategoryManager::getObject(sim_track, geant_kine_, i);
     int parent_track_id = sim_track->getParentTrack();
     bool parent_is_selected = std::count( selected_tracks.begin(), selected_tracks.end(), parent_track_id ) != 0;
-    if( !sim_tracks->isPrimary() && !parent_is_selected )
+    if( !sim_track->isPrimary() && !parent_is_selected )
       continue;
     selected_tracks.push_back( sim_track->getTrack() );
     float pt = sim_track->getTransverseMomentum() / 1000.; // MeV->GeV
