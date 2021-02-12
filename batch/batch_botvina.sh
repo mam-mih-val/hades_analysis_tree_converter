f4b1f6451c1979532f0318ec0a5e0199c9982cd1
mam-mih-val <mam.mih.val@gmail.com>
2021-02-12 15:11:32 +0300

    batching scripts for botvina files

---
+++ batch/batch_botvina.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+file_list=$1
+output_dir=$2
+config_file=$3
+
+hadesroot=/cvmfs/hades.gsi.de/install/6.12.06/hydra2-5.3/defall.sh
+
+current_dir=$(pwd)
+partition=main
+time=8:00:00
+build_dir=$current_dir/../build
+
+lists_dir=${output_dir}/lists/
+log_dir=${output_dir}/log
+
+mkdir -p $output_dir
+mkdir -p $log_dir
+mkdir -p $lists_dir
+
+split -l 1 -d -a 3 --additional-suffix=.list "$file_list" $lists_dir
+
+n_runs=$(ls $lists_dir/*.list | wc -l)
+
+job_range=1-$n_runs
+
+echo file list=$file_list
+echo output_dir=$output_dir
+echo log_dir=$log_dir
+echo lists_dir=$lists_dir
+echo n_runs=$n_runs
+echo job_range=$job_range
+
+sbatch -J HT2AT -p $partition -t $time -a $job_range -e ${log_dir}/%A_%a.e -o ${log_dir}/%A_%a.o --export=output_dir=$output_dir,file_list=$file_list,hadesroot=$hadesroot,lists_dir=$lists_dir,build_dir=$build_dir,config_file=$config_file -- /cvmfs/vae.gsi.de/debian8/containers/debian8-user_container_20210211T1503.sif /lustre/nyx/hades/user/mmamaev/hades_analysis_tree_converter/batch/batch_botvina_run.sh
---
+++ batch/batch_botvina_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+format='+%Y/%m/%d-%H:%M:%S'
+
+date $format
+
+job_num=$(($SLURM_ARRAY_TASK_ID))
+
+filelist=$lists_dir/$(ls $lists_dir | sed "${job_num}q;d")
+
+input_files=`head -n 1 $filelist`
+
+cd $output_dir
+mkdir -p $job_num
+cd $job_num
+
+echo "loading " $hadesroot
+source $hadesroot
+
+echo "executing $build_dir/HT2AT -i $input_files -o output.root -c $config_file -m"
+$build_dir/HT2AT -i $input_files -o output.root -c $config_file -m
+
+directory=`dirname $filelist`
+
+file=$directory/../geant_out1.root
+
+echo "executing $build_dir/HT2AT -i $input_files -o output.root -c $config_file -m"
+$build_dir/convert_wall -i $file -o wall_output.root -c $config_file
+
+echo JOB FINISHED!
+date $format
